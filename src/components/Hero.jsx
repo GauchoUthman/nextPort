67244594df52716967233c6bc2070b128128a810
GauchoUthman <96721266+GauchoUthman@users.noreply.github.com>
2023-04-18 13:36:17 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,16 +10,16 @@ const Hero = () => {
   return (
     <section id="home" className="overflow-hidden">
       <div className="min-h-screen relative flex md:flex-row flex-col-reverse md:items-end justify-center items-center">
-      {/*  <div
+       <div
           data-aos="slide-left"
           data-aos-delay="1200"
           className="absolute h-full md:w-4/12 w-8/12 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
         >
-          <h1 className="rotate-90 absolute top-[30%] right-[-15%] text-[#EAF2FA]">
+         {/* <h1 className="rotate-90 absolute top-[30%] right-[-15%] text-[#EAF2FA]">
             Jurgen
             <span className="text-dark_primary">Klop</span>
-          </h1>
-        </div>*/}
+          </h1>*/}
+        </div>
 
         {/* first col */}
         <div className="pb-16 px-6 pt-5" data-aos="fade-down">
