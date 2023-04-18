8117fd4157568979293949160cb48a75e7c3a1d7
GauchoUthman <96721266+GauchoUthman@users.noreply.github.com>
2023-04-18 13:49:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,7 @@ const App = () => {
       <Testimonials />
       <Hireme />
       <Contact />*/}
-      <footer className="p-3 text-center">
-        <h6 className="mb-3">JOHN ALEX</h6>
-        <p>codeaprogram © All CopyRights Reserved 2022</p>
-      </footer>
+      
     </div>
   );
 };
