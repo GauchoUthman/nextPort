ea3fc640751eb11d0de867499a501d39ec182613
GauchoUthman <96721266+GauchoUthman@users.noreply.github.com>
2023-04-18 12:27:04 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 // import content
 import { useEffect } from "react";
-import { content } from "../Content";
+//import { content } from "../Content";
 const Hero = () => {
   const { hero } = content;
 
@@ -13,19 +13,19 @@ const Hero = () => {
           className="absolute h-full md:w-4/12 w-8/12 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
         >
           <h1 className="rotate-90 absolute top-[30%] right-[-15%] text-[#EAF2FA]">
-            {hero.firstName}{" "}
-            <span className="text-dark_primary">{hero.LastName}</span>
+            Jurgen
+            <span className="text-dark_primary">Klop</span>
           </h1>
         </div>
 
         {/* first col */}
         <div className="pb-16 px-6 pt-5" data-aos="fade-down">
-          <h2>{hero.title}</h2>
+          <h2>Liverpool</h2>
           <br />
           <div className="flex justify-end">
-            <button className="btn">{hero.btnText}</button>
+            <button className="btn">Explore</button>
           </div>
-          <div className="flex flex-col gap-10 mt-10">
+        {/*  <div className="flex flex-col gap-10 mt-10">
             {hero.hero_content.map((content, i) => (
               <div
                 key={i}
@@ -38,7 +38,7 @@ const Hero = () => {
                 <p>{content.text}</p>
               </div>
             ))}
-          </div>
+          </div>*/}
         </div>
 
         {/* sec col */}
